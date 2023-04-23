abf26735ac35d39bcff00d4b7e2128ad94ca7020
kim0hoon <gns9460@gmail.com>
2023-04-23 22:51:17 +0900

    [FIX] MainActivity onCreate 매개변수 수정

--- app/src/main/java/com/polzzak_android/common/MainActivity.kt
+++ app/src/main/java/com/polzzak_android/common/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.polzzak_android.common
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import com.polzzak_android.R
 import com.polzzak_android.common.base.BaseActivity
 import com.polzzak_android.databinding.ActivityMainBinding
@@ -9,7 +8,7 @@ import com.polzzak_android.databinding.ActivityMainBinding
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override val layoutResId: Int = R.layout.activity_main
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 }
