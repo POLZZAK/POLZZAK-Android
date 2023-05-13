4b50ddb7c28edc83349093b69eaa2856bc9d3f2b
kim0hoon <gns9460@gmail.com>
2023-05-13 16:57:34 +0900

    [REFACTOR] 로그인 부분 분리, Timber 적용 https://github.com/POLZZAK/POLZZAK-Android/issues/3

--- app/src/main/java/com/polzzak_android/common/MainActivity.kt
+++ app/src/main/java/com/polzzak_android/common/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.polzzak_android.common
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
@@ -13,20 +8,11 @@ import androidx.fragment.app.FragmentManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.common.KakaoSdk
-import com.kakao.sdk.common.model.ClientError
-import com.kakao.sdk.common.model.ClientErrorCause
-import com.kakao.sdk.user.UserApiClient
-import com.kakao.sdk.user.model.User
-import com.polzzak_android.BuildConfig
 import com.polzzak_android.R
 import com.polzzak_android.common.base.BaseActivity
 import com.polzzak_android.common.model.SocialLoginType
+import com.polzzak_android.common.sociallogin.GoogleLoginHelper
+import com.polzzak_android.common.sociallogin.KakaoLoginHelper
 import com.polzzak_android.databinding.ActivityMainBinding
 import com.polzzak_android.presentation.login.LoginFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,13 +20,12 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(), FragmentOwner, SocialLoginManager {
     override val layoutResId: Int = R.layout.activity_main
-    private var mGoogleSignInClient: GoogleSignInClient? = null
-    private var resultLauncher: ActivityResultLauncher<Intent>? = null
-
     private val mainViewModel by viewModels<MainViewModel>()
 
     private lateinit var navController: NavController
 
+    private var googleLoginHelper: GoogleLoginHelper? = null
+    private var kakaoLoginHelper: KakaoLoginHelper? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
@@ -55,8 +40,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), FragmentOwner, SocialL
         btmNav.setupWithNavController(navController)
 
         initLoginFragmentResultListener()
-        initGoogleLogin()
-        initKakaoLogin()
+        initLoginHelper()
         openLoginFragment()
     }
 
@@ -78,28 +62,27 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), FragmentOwner, SocialL
         supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 
-    private fun initGoogleLogin() {
-        resultLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-                if (result.resultCode == Activity.RESULT_OK) {
-                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                    val account = task.getResult(ApiException::class.java)
-                    val id = account?.id
-                    id?.let {
-                        mainViewModel.requestLogin(
-                            id = it,
-                            loginType = SocialLoginType.GOOGLE
-                        )
-                    }
+    private fun initLoginHelper() {
+        googleLoginHelper = GoogleLoginHelper(activity = this).apply {
+            setLoginSuccessCallback {
+                it.id?.let { id ->
+                    mainViewModel.requestLogin(
+                        id = id,
+                        loginType = SocialLoginType.GOOGLE
+                    )
                 }
             }
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail()
-            .requestEmail().requestId().build()
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-    }
-
-    private fun initKakaoLogin() {
-        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+        }
+        kakaoLoginHelper = KakaoLoginHelper(context = this).apply {
+            setLoginSuccessCallback {
+                it.id?.let { id ->
+                    mainViewModel.requestLogin(
+                        id.toString(),
+                        loginType = SocialLoginType.KAKAO
+                    )
+                }
+            }
+        }
     }
 
     private fun initLoginFragmentResultListener() {
@@ -122,68 +105,12 @@ class MainActivity : BaseActivity<ActivityMainBinding>(), FragmentOwner, SocialL
         openFragment(loginFragment)
     }
 
-
     override fun requestLoginGoogle() {
-        val signInIntent = mGoogleSignInClient?.signInIntent
-        val lastAccount = GoogleSignIn.getLastSignedInAccount(this)
-        lastAccount?.let {
-            val id = it.id ?: return@let null
-            mainViewModel.requestLogin(id = id, loginType = SocialLoginType.GOOGLE)
-        } ?: run {
-            resultLauncher?.launch(signInIntent)
-        }
+        googleLoginHelper?.requestLogin()
     }
 
     override fun requestLoginKakao() {
-        //TODO 로그인 기능 정리하여 파일 이동
-        with(UserApiClient.instance) {
-//            logout{
-//                me(callback = kakaoLoginSuccessCallback)
-//                Log.d("MainActivity","logout : $it")
-//            }
-
-//            unlink{
-//                me(callback = kakaoLoginSuccessCallback)
-//                Log.d("MainActivity","unlink : $it")
-//            }
-
-            val kakaoLoginSuccessCallback: (User?, Throwable?) -> Unit = { user, error ->
-                Log.d("MainActivity", "유저 id : ${user?.id}")
-                //TODO 로그인 성공 콜백 구현
-            }
-            val kakaoLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-                if (error != null) {
-                    Log.e("MainActivity", "카카오로 로그인 실패", error)
-                } else if (token != null) {
-                    Log.i("MainActivity", "카카오로 로그인 성공 ${token.accessToken}")
-                    me(callback = kakaoLoginSuccessCallback)
-                }
-            }
-            val kakaoTalkLoginCallback: (OAuthToken?, Throwable?) -> Unit =
-                kakaoTalkLoginCallback@{ token, error ->
-                    if (error != null) {
-                        Log.e("MainActivity", "카카오톡 계정으로 로그인 실패", error)
-                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) return@kakaoTalkLoginCallback
-                        loginWithKakaoAccount(this@MainActivity, callback = kakaoLoginCallback)
-                    } else if (token != null) {
-                        Log.i(
-                            "MainActivity",
-                            "카카오톡 계정으로 로그인 성공 ${token.accessToken}"
-                        )
-                        me(callback = kakaoLoginSuccessCallback)
-                    }
-                }
-
-            if (isKakaoTalkLoginAvailable(this@MainActivity)) {
-                //카카오톡으로 로그인
-                loginWithKakaoTalk(this@MainActivity, callback = kakaoTalkLoginCallback)
-            } else {
-                //카카오계정으로 로그인
-                loginWithKakaoAccount(this@MainActivity, callback = kakaoLoginCallback)
-            }
-        }
+        kakaoLoginHelper?.requestLogin()
     }
 
     companion object {
