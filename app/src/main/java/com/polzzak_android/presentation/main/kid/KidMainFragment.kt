e45503122eee66d0caff9feb683e27f68bb57766
taehee28 <dev.thk28@gmail.com>
2023-05-28 15:52:30 +0900

    [FEAT] 아이 화면 Navigation graph 구성
    issue: #18

---
+++ app/src/main/java/com/polzzak_android/presentation/main/kid/KidMainFragment.kt
@@ -0,0 +1,10 @@
+package com.polzzak_android.presentation.main.kid
+
+import com.polzzak_android.R
+import com.polzzak_android.common.base.BaseFragment
+import com.polzzak_android.databinding.FragmentKidMainBinding
+
+class KidMainFragment : BaseFragment<FragmentKidMainBinding>() {
+    override val layoutResId: Int = R.layout.fragment_kid_main
+
+}
