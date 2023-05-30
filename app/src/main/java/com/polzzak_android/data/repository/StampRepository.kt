8fc12602385c40574794b6c33274cc815efacf59
taehee28 <dev.thk28@gmail.com>
2023-05-30 21:18:20 +0900

    [FEAT] 메인 도장판 화면용 StampRepository 정의 및 mock 클래스 구현
    issue: #24
    * 도장판 목록 api 응답으로 사용할 StampBoardListResponse 정의
    * StampRespository 인터페이스 정의
    * fake data 반환하는 MockStampRepositoryImpl 구현

---
+++ app/src/main/java/com/polzzak_android/data/repository/StampRepository.kt
@@ -0,0 +1,67 @@
+package com.polzzak_android.data.repository
+
+import com.polzzak_android.common.model.ApiResult
+import com.polzzak_android.data.remote.model.StampBoardGroup
+import com.polzzak_android.data.remote.model.response.StampBoardListResponse
+
+interface StampRepository {
+    /**
+     * 도장판 목록 리스트 요청
+     */
+    suspend fun getStampBoardList(
+        stampBoardGroup: StampBoardGroup,
+        memberId: String = ""
+    ): ApiResult<List<StampBoardListResponse.StampBoardListResponseData>>
+}
+
+/**
+ * fake data 받기 위한 구현클래스
+ */
+class MockStampRepositoryImpl : StampRepository {
+    override suspend fun getStampBoardList(
+        stampBoardGroup: StampBoardGroup,
+        memberId: String
+    ): ApiResult<List<StampBoardListResponse.StampBoardListResponseData>> {
+        val fakeData = listOf(
+            StampBoardListResponse.StampBoardListResponseData(
+                partner = StampBoardListResponse.StampBoardListResponseData.PartnerData(
+                    memberId = 11,
+                    nickname = "아이 1",
+                    memberType = "KID",
+                    profileUrl = "url"
+                ),
+                stampBoardSummaries = listOf(
+                    StampBoardListResponse.StampBoardListResponseData.StampBoardSummaryData(
+                        stampBoardId = 1,
+                        name = "도장판 1",
+                        currentStampCount = 13,
+                        goalStampCount = 30,
+                        reward = "콘서트",
+                        missionRequestCount = 5,
+                        status = "progress"
+                    ),
+                    StampBoardListResponse.StampBoardListResponseData.StampBoardSummaryData(
+                        stampBoardId = 3,
+                        name = "도장판 3",
+                        currentStampCount = 30,
+                        goalStampCount = 30,
+                        reward = "칭찬",
+                        missionRequestCount = 0,
+                        status = "completed"
+                    )
+                )
+            ),
+            StampBoardListResponse.StampBoardListResponseData(
+                partner = StampBoardListResponse.StampBoardListResponseData.PartnerData(
+                    memberId = 12,
+                    nickname = "아이 2",
+                    memberType = "KID",
+                    profileUrl = "image_url"
+                ),
+                stampBoardSummaries = emptyList()
+            )
+        )
+
+        return ApiResult.success(fakeData)
+    }
+}
