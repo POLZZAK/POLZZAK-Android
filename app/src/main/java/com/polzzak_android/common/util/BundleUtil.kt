425e87579836e429ad13400dafbd9b4f94416ca1
kim0hoon <gns9460@gmail.com>
2023-06-05 01:59:16 +0900

    [FEAT] Parcelable 관련 유틸 추가

--- app/src/main/java/com/polzzak_android/common/util/BundleUtil.kt
+++ app/src/main/java/com/polzzak_android/common/util/BundleUtil.kt
@@ -3,19 +3,35 @@ package com.polzzak_android.common.util
 import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
-import com.polzzak_android.presentation.auth.signup.SignUpFragment
 import timber.log.Timber
 
 fun <T : Parcelable> Bundle.getParcelableOrNull(key: String, clazz: Class<T>): T? {
     return try {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            getParcelable(SignUpFragment.ARGUMENT_SOCIAL_LOGIN_TYPE_KEY, clazz)
+            getParcelable(key, clazz)
         } else {
             @Suppress("DEPRECATION")
-            getParcelable(SignUpFragment.ARGUMENT_SOCIAL_LOGIN_TYPE_KEY) as? T
+            getParcelable(key)
         }
     } catch (e: Exception) {
         Timber.e("getParcelableOrNull(key = $key, clazz : $clazz) error $e")
+        null
+    }
+}
+
+fun <T : Parcelable> Bundle.getParcelableArrayListOrNull(
+    key: String,
+    clazz: Class<T>
+): ArrayList<T>? {
+    return try {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            getParcelableArrayList(key, clazz)
+        } else {
+            @Suppress("DEPRECATION")
+            getParcelableArrayList(key)
+        }
+    } catch (e: Exception) {
+        Timber.e("getParcelableArrayListOrNull(key = $key, clazz : $clazz) error $e")
         null
     }
 }
