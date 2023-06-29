0b1ebff1d15afebd87137ce6c93d83dc3eb6d420
kim0hoon <gns9460@gmail.com>
2023-06-29 23:17:16 +0900

    [FEAT] 찾기 메인페이지 디자인 적용 https://github.com/POLZZAK/POLZZAK-Android/issues/34

--- app/src/main/java/com/polzzak_android/presentation/link/search/protector/ProtectorSearchOptionMenuFragment.kt
+++ app/src/main/java/com/polzzak_android/presentation/link/search/protector/ProtectorSearchOptionMenuFragment.kt
@@ -7,11 +7,10 @@ class ProtectorSearchOptionMenuFragment : BaseSearchOptionMenuFragment() {
     //TODO 임시 drawable -> 디자인가이드 적용 필요
     override val iconDrawableRes: Int = R.drawable.logo_third
 
-    //TODO string resource로 변경
-    override val contentString: String = "칭찬 도장판을 만들려면\n아이와 연동이 필요해요"
+    override val contentStringRes: Int = R.string.search_main_parent_content
     override val actionNavigateHostFragment: Int =
         R.id.action_protectorSearchOptionMenuFragment_to_protectorHostFragment
     override val actionNavigateSearchFragment: Int =
         R.id.action_protectorSearchOptionMenuFragment_to_protectorSearchFragment
-    override val searchButtonText: String = "보호자 찾기"
+    override val searchButtonTextRes: Int = R.string.common_search_parent
 }
