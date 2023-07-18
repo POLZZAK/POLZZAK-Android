400c3436657f25c501adbeb24db58f1a3896823c
taehee28 <dev.thk28@gmail.com>
2023-07-18 18:30:28 +0900

    [REFACTOR] 부모/아이 쿠폰 상세 화면 컴포저블 파라미터 변경
    issue: #64
    
    * 파라미터를 CouponDetailModel에서 StateFlow<ModelState<CouponDetailModel>>로 변경

--- app/src/main/java/com/polzzak_android/presentation/feature/coupon/detail/CouponDetailScreen.kt
+++ app/src/main/java/com/polzzak_android/presentation/feature/coupon/detail/CouponDetailScreen.kt
@@ -15,100 +15,78 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.polzzak_android.presentation.common.compose.Blue500
 import com.polzzak_android.presentation.common.compose.Blue600
+import com.polzzak_android.presentation.common.model.ModelState
 import com.polzzak_android.presentation.component.PolzzakButton
 import com.polzzak_android.presentation.component.PolzzakWhiteButton
 import com.polzzak_android.presentation.component.polzzakButtonColors
 import com.polzzak_android.presentation.coupon.model.CouponState
 import com.polzzak_android.presentation.feature.coupon.model.CouponDetailModel
+import kotlinx.coroutines.flow.StateFlow
 import java.time.Duration
 import java.time.LocalDate
 
 @Composable
 fun CouponDetailScreen_Protector(
-    data: CouponDetailModel,
+    couponDetailData: StateFlow<ModelState<CouponDetailModel>>,
     onMissionClick: () -> Unit
 ) {
-    CouponDetailScreen(data = data, onMissionClick = onMissionClick)
-}
-
-@Preview
-@Composable
-private fun CouponDetailScreen_ProtectorPreview() {
-    CouponDetailScreen_Protector(
-        data = CouponDetailModel(
-            couponId = 0,
-            state = CouponState.ISSUED,
-            rewardTitle = "쿠폰 샘플",
-            giverName = "홍길동",
-            giverProfileUrl = "",
-            receiverName = "김철수",
-            receiverProfileUrl = "",
-            missions = listOf("1", "2", "3"),
-            stampCount = 16,
-            startDate = LocalDate.now().minusDays(7),
-            endDate = LocalDate.now()
-        ),
-        onMissionClick = {},
-    )
+    val state by couponDetailData.collectAsState()
+    
+    when (state) {
+        is ModelState.Loading -> {}
+        is ModelState.Success -> {
+            CouponDetailScreen(data = state.data!!, onMissionClick = onMissionClick)
+        }
+        is ModelState.Error -> {}
+    }
 }
 
 @Composable
 fun CouponDetailScreen_Kid(
-    data: CouponDetailModel,
+    couponDetailData: StateFlow<ModelState<CouponDetailModel>>,
     onMissionClick: () -> Unit,
     onRewardRemindClick: () -> Unit,
     onRewardDeliveredClick: () -> Unit
 ) {
-    CouponDetailScreen(
-        data = data,
-        onMissionClick = onMissionClick,
-        buttons = {
-            // TODO: 선물 조르기 요청 상태에 따라 버튼 종류가 달라짐
-            PolzzakButton(
-                onClick = onRewardRemindClick,
-                colors = ButtonDefaults.polzzakButtonColors(backgroundColor = Blue600),
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(text = "선물 조르기")
-            }
-            Spacer(modifier = Modifier.width(7.dp))
-            PolzzakWhiteButton(
-                onClick = onRewardDeliveredClick,
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(text = "선물 받기 완료")
-            }
-        }
-    )
-}
+    val state by couponDetailData.collectAsState()
 
-@Preview
-@Composable
-private fun CouponDetailScreen_KidPreview() {
-    CouponDetailScreen_Kid(
-        data = CouponDetailModel(
-            couponId = 0,
-            state = CouponState.ISSUED,
-            rewardTitle = "쿠폰 샘플",
-            giverName = "홍길동",
-            giverProfileUrl = "",
-            receiverName = "김철수",
-            receiverProfileUrl = "",
-            missions = listOf("1", "2", "3"),
-            stampCount = 16,
-            startDate = LocalDate.now().minusDays(7),
-            endDate = LocalDate.now()
-        ),
-        onMissionClick = {},
-        onRewardDeliveredClick = {},
-        onRewardRemindClick = {}
-    )
+    when (state) {
+        is ModelState.Loading -> {}
+        is ModelState.Success -> {
+            CouponDetailScreen(
+                data = state.data!!,
+                onMissionClick = onMissionClick,
+                buttons = {
+                    // TODO: 선물 조르기 요청 상태에 따라 버튼 종류가 달라짐
+                    PolzzakButton(
+                        onClick = onRewardRemindClick,
+                        colors = ButtonDefaults.polzzakButtonColors(backgroundColor = Blue600),
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(text = "선물 조르기")
+                    }
+                    Spacer(modifier = Modifier.width(7.dp))
+                    PolzzakWhiteButton(
+                        onClick = onRewardDeliveredClick,
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(text = "선물 받기 완료")
+                    }
+                }
+            )
+        }
+        is ModelState.Error -> {}
+    }
+
+
 }
 
 @Composable
@@ -172,4 +150,42 @@ private fun CouponDetailScreen(
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun CouponDetailScreenPreview() {
+    CouponDetailScreen(
+        data = CouponDetailModel(
+            couponId = 0,
+            state = CouponState.ISSUED,
+            rewardTitle = "쿠폰 샘플",
+            giverName = "홍길동",
+            giverProfileUrl = "",
+            receiverName = "김철수",
+            receiverProfileUrl = "",
+            missions = listOf("1", "2", "3"),
+            stampCount = 16,
+            startDate = LocalDate.now().minusDays(7),
+            endDate = LocalDate.now()
+        ),
+        onMissionClick = {},
+        buttons = {
+            // TODO: 선물 조르기 요청 상태에 따라 버튼 종류가 달라짐
+            PolzzakButton(
+                onClick = {},
+                colors = ButtonDefaults.polzzakButtonColors(backgroundColor = Blue600),
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "선물 조르기")
+            }
+            Spacer(modifier = Modifier.width(7.dp))
+            PolzzakWhiteButton(
+                onClick = {},
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "선물 받기 완료")
+            }
+        }
+    )
 }
