49c843a06edd87e83a03903a8afed25923fc7a21
taehee28 <dev.thk28@gmail.com>
2023-08-01 20:37:18 +0900

    [RENAME] StampModule 클래스 이름 변경
    * StampModule -> RepositoryModule

--- app/src/main/java/com/polzzak_android/data/di/RepositoryModule.kt
+++ app/src/main/java/com/polzzak_android/data/di/RepositoryModule.kt
@@ -9,7 +9,7 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class StampModule {
+abstract class RepositoryModule {
     @Binds
     abstract fun bindStampRepository(mockStampRepositoryImpl: MockStampRepositoryImpl): StampRepository
 }
