fec77e78c1a686671cd31845a6df70df17e5070f
taehee28 <dev.thk28@gmail.com>
2023-08-09 17:37:28 +0900

    [FEAT] CouponRepository, CouponDetailViewModel 클래스 추가
    issue: #72

--- app/src/main/java/com/polzzak_android/data/repository/CouponRepository.kt
+++ app/src/main/java/com/polzzak_android/data/repository/CouponRepository.kt
@@ -2,11 +2,10 @@ package com.polzzak_android.data.repository
 
 import com.polzzak_android.data.remote.model.ApiResult
 import com.polzzak_android.data.remote.model.response.CouponListResponse
-import com.polzzak_android.data.remote.model.response.StampBoardListResponse
 import com.polzzak_android.data.remote.service.CouponService
 import com.polzzak_android.data.remote.util.createHeaderAuthorization
 import com.polzzak_android.data.remote.util.requestCatching
-import retrofit2.Response
+import com.polzzak_android.data.remote.model.response.CouponDetailDto
 import javax.inject.Inject
 
 interface CouponRepository {
@@ -15,6 +14,11 @@ interface CouponRepository {
         couponState: String,
         partnerMemberId: Int?
     ): ApiResult<List<CouponListResponse.Data>>
+
+    suspend fun getCouponDetail(
+        token: String,
+        couponId: Int
+    ): ApiResult<CouponDetailDto>
 }
 
 
@@ -29,5 +33,31 @@ class CouponRepositoryImpl @Inject constructor(
     ): ApiResult<List<CouponListResponse.Data>> = requestCatching {
         val accessToken = createHeaderAuthorization(token)
         couponService.getCouponList(accessToken, couponState, partnerMemberId)
+    }
+
+    override suspend fun getCouponDetail(
+        token: String,
+        couponId: Int
+    ): ApiResult<CouponDetailDto> {
+        val fake = CouponDetailDto(
+            couponId = 1,
+            reward = "상상상",
+            guardian = CouponDetailDto.ProfileDto(
+                nickname = "보호자",
+                profileUrl = "profile url"
+            ),
+            kid = CouponDetailDto.ProfileDto(
+                nickname = "아이",
+                profileUrl = "profile url"
+            ),
+            missionContents = listOf("미션1", "미션2", "미션3"),
+            stampCount = 10,
+            state = "ISSUED",
+            rewardRequestDate = "2023-07-31T13:59:43.659572713",
+            startDate = "2023-07-31T15:59:43.659589415",
+            endDate = "2023-07-31T15:59:43.659595016"
+        )
+
+        return ApiResult.success(fake)
     }
 }
---
+++ app/src/main/java/com/polzzak_android/presentation/feature/coupon/detail/CouponDetailViewModel.kt
@@ -0,0 +1,54 @@
+package com.polzzak_android.presentation.feature.coupon.detail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.polzzak_android.data.repository.CouponRepository
+import com.polzzak_android.presentation.common.model.ModelState
+import com.polzzak_android.presentation.feature.coupon.model.CouponDetailModel
+import com.polzzak_android.presentation.feature.coupon.model.toModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class CouponDetailViewModel @Inject constructor(
+    private val repository: CouponRepository
+) : ViewModel() {
+    private val _couponDetailData = MutableStateFlow<ModelState<CouponDetailModel>>(ModelState.Loading())
+    val couponDetailData
+        get() = _couponDetailData.asStateFlow()
+
+    fun fetchCouponDetailData(
+        token: String,
+        couponId: Int
+    ) = viewModelScope.launch {
+        repository
+            .getCouponDetail(token = token, couponId = couponId)
+            .onSuccess { data ->
+                data ?: return@onSuccess
+
+                _couponDetailData.update {
+                    ModelState.Success(data.toModel())
+                }
+            }
+            .onError { exception, data ->
+                Timber.e(exception)
+
+                _couponDetailData.update {
+                    ModelState.Error(exception = exception)
+                }
+            }
+    }
+
+    fun requestReward(couponId: Int) {
+        // TODO: 조르기 api 호출 처리
+    }
+
+    fun receiveReward(couponId: Int) {
+        // TODO: 보상 받음 api 호출 처리
+    }
+ }
