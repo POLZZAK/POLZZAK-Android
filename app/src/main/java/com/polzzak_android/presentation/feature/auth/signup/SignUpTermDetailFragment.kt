01510cd4f096d48b9bedc30c99d1c3daa88f4b8a
kim0hoon <gns9460@gmail.com>
2023-08-27 21:09:54 +0900

    [FEAT] 공용툴바로 변경 https://github.com/POLZZAK/POLZZAK-Android/issues/19

--- app/src/main/java/com/polzzak_android/presentation/feature/auth/signup/SignUpTermDetailFragment.kt
+++ app/src/main/java/com/polzzak_android/presentation/feature/auth/signup/SignUpTermDetailFragment.kt
@@ -5,18 +5,28 @@ import androidx.navigation.fragment.findNavController
 import com.polzzak_android.R
 import com.polzzak_android.databinding.FragmentSignupTermDetailBinding
 import com.polzzak_android.presentation.common.base.BaseFragment
+import com.polzzak_android.presentation.component.toolbar.ToolbarData
+import com.polzzak_android.presentation.component.toolbar.ToolbarHelper
 
 class SignUpTermDetailFragment : BaseFragment<FragmentSignupTermDetailBinding>() {
     override val layoutResId: Int = R.layout.fragment_signup_term_detail
 
     override fun initView() {
         super.initView()
-        with(binding) {
-            ivBtnBack.setOnClickListener {
-                findNavController().popBackStack()
+        initToolbar()
+        initWebView()
+    }
+
+    private fun initToolbar() {
+        with(binding.inToolbar) {
+            val title = arguments?.getString(ARGUMENT_TITLE_KEY) ?: ""
+            val toolbarData =
+                ToolbarData(popStack = findNavController(), titleText = title)
+            ToolbarHelper(data = toolbarData, toolbar = this).run {
+                set()
+                updateToolbarBackgroundColor(R.color.white)
             }
         }
-        initWebView()
     }
 
     private fun initWebView() {
