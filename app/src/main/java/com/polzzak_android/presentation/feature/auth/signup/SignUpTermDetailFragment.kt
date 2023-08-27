282fdbf36ef83dfd228e1d29d7750e168110bfde
kim0hoon <gns9460@gmail.com>
2023-08-27 19:51:00 +0900

    [FEAT] 약관 웹뷰 추가 https://github.com/POLZZAK/POLZZAK-Android/issues/19

--- app/src/main/java/com/polzzak_android/presentation/feature/auth/signup/SignUpTermDetailFragment.kt
+++ app/src/main/java/com/polzzak_android/presentation/feature/auth/signup/SignUpTermDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.polzzak_android.presentation.feature.auth.signup
 
+import android.webkit.WebViewClient
 import androidx.navigation.fragment.findNavController
 import com.polzzak_android.R
 import com.polzzak_android.databinding.FragmentSignupTermDetailBinding
@@ -11,18 +12,25 @@ class SignUpTermDetailFragment : BaseFragment<FragmentSignupTermDetailBinding>()
     override fun initView() {
         super.initView()
         with(binding) {
-            val title = arguments?.getString(ARGUMENT_TITLE_KEY) ?: ""
-            val content = arguments?.getString(ARGUMENT_CONTENT_KEY) ?: ""
-            tvTitle.text = title
-            tvContent.text = content
             ivBtnBack.setOnClickListener {
                 findNavController().popBackStack()
             }
+        }
+        initWebView()
+    }
+
+    private fun initWebView() {
+        with(binding.wbContent) {
+            val url = arguments?.getString(ARGUMENT_URL_KEY) ?: ""
+            webViewClient = WebViewClient()
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            loadUrl(url)
         }
     }
 
     companion object {
         const val ARGUMENT_TITLE_KEY = "argument_title_key"
-        const val ARGUMENT_CONTENT_KEY = "argument_content_key"
+        const val ARGUMENT_URL_KEY = "argument_url_key"
     }
 }
