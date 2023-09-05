47b21cb8bd9d2cd600652832b915aac9fa77eecd
HS0204 <lhanseul0204@naver.com>
2023-09-05 21:49:31 +0900

    [CHORE] 바텀시트 title, subtitle spannable 수정

--- app/src/main/java/com/polzzak_android/presentation/component/bottomsheet/CommonBottomSheetModel.kt
+++ app/src/main/java/com/polzzak_android/presentation/component/bottomsheet/CommonBottomSheetModel.kt
@@ -1,11 +1,12 @@
 package com.polzzak_android.presentation.component.bottomsheet
 
+import android.text.Spannable
 import com.polzzak_android.presentation.common.model.CommonButtonModel
 
 data class CommonBottomSheetModel(
     val type: BottomSheetType,
-    val title: String,
-    val subTitle: String? = null,
+    val title: Spannable,
+    val subTitle: Spannable? = null,
     val contentList: List<*>,
     val button: CommonButtonModel
 )
@@ -14,7 +15,8 @@ data class CommonBottomSheetModel(
  * MISSION : 미션
  * PROFILE_IMAGE : 프로필 이미지형
  * SELECT_STAMP_BOARD : 도장판 조회
+ * COUPON : 쿠폰
  */
 enum class BottomSheetType {
-    MISSION, PROFILE_IMAGE, SELECT_STAMP_BOARD
+    MISSION, PROFILE_IMAGE, SELECT_STAMP_BOARD, COUPON
 }
