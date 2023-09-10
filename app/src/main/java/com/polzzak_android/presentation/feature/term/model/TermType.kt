d83fec8e4d332aadfaaab98df9c58fe8d12a0b81
kim0hoon <gns9460@gmail.com>
2023-09-11 00:39:52 +0900

    [FEAT] 약관 url 적용 https://github.com/POLZZAK/POLZZAK-Android/issues/19

--- app/src/main/java/com/polzzak_android/presentation/feature/term/model/TermType.kt
+++ app/src/main/java/com/polzzak_android/presentation/feature/term/model/TermType.kt
@@ -5,9 +5,8 @@ import androidx.annotation.StringRes
 import com.polzzak_android.R
 import kotlinx.parcelize.Parcelize
 
-//TODO 웹뷰 주소
 @Parcelize
 enum class TermType(@StringRes val titleStrRes: Int, val url: String) : Parcelable {
-    SERVICE(R.string.terms_of_service_detail_service_title, "http://google.co.kr"),
-    PRIVACY(R.string.terms_of_service_detail_privacy_title, "http://naver.com"),
+    SERVICE(R.string.terms_of_service_detail_service_title, "https://www.polzzak.co.kr/terms "),
+    PRIVACY(R.string.terms_of_service_detail_privacy_title, "https://www.polzzak.co.kr/privacy"),
 }
