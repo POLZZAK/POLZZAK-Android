4e2a58974e4ceade32a48cb6d5ea42bd43c082fb
kim0hoon <gns9460@gmail.com>
2023-09-13 00:17:24 +0900

    [FEAT] 알림 시간 변환함수 구현 https://github.com/POLZZAK/POLZZAK-Android/issues/68

--- app/src/main/java/com/polzzak_android/presentation/common/util/Extensions.kt
+++ app/src/main/java/com/polzzak_android/presentation/common/util/Extensions.kt
@@ -5,7 +5,6 @@ import java.time.Duration
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.Period
-import java.time.format.DateTimeFormatter
 
 
 /**
@@ -47,11 +46,36 @@ fun LocalDateTime.getRemainingSeconds(): Int {
 fun LocalDate.toPublishedDateString(): String {
     val nowDate = LocalDate.now()
     val diff = Period.between(this, nowDate)
-    val getDateStr = { date: Int -> String.format("%02d", date) }
     return when {
-        this.isAfter(nowDate.minusMonths(3)) -> "${getDateStr(this.monthValue)}.${getDateStr(this.dayOfMonth)}"
+        this.isAfter(nowDate.minusMonths(3)) -> "${getTimeFormat(this.monthValue)}.${
+            getTimeFormat(
+                this.dayOfMonth
+            )
+        }"
+
         this.isBefore(nowDate.minusYears(1)) || this.isEqual(nowDate.minusYears(1)) -> "${diff.years}년 전"
         else -> "${diff.months}개월 전"
     }
 }
 
+/**
+ * 알림 날짜 표시 규칙
+ * 0초~59초 => 방금 전
+ * 1시간 이내는 ‘분’ 표시 => 1분 전 ~ 59분 전
+ * 하루 지나기 전까지는 ‘시간’ 표시 => 1시간 ~ 23시간 전
+ * 하루 지나고 3일까지는 ‘일’ 표시 => 1일 전 ~ 3일 전
+ * 3일 이후부터는 월.일 표시 => ex) 05.08
+ */
+fun LocalDateTime.toNotificationDateString(): String {
+    val nowDate = LocalDateTime.now()
+    val diff = Duration.between(this, nowDate)
+    return when {
+        this.isAfter(nowDate.minusMinutes(1)) -> "방금 전"
+        this.isAfter(nowDate.minusHours(1)) -> "${maxOf(1, diff.toMinutes())}분 전"
+        this.isAfter(nowDate.minusDays(1)) -> "${maxOf(1, diff.toHours())}시간 전"
+        this.isAfter(nowDate.minusDays(3)) -> "${maxOf(1, diff.toDays())}일 전"
+        else -> "${getTimeFormat(this.monthValue)}.${getTimeFormat(this.dayOfMonth)}"
+    }
+}
+
+private fun getTimeFormat(t: Int) = String.format("%02d", t)
