001b71d24144e24230d3154403d0c449a6e4ac3c
kim0hoon <gns9460@gmail.com>
2023-09-16 15:05:02 +0900

    signUpPage Type 변경(적용필요) https://github.com/POLZZAK/POLZZAK-Android/issues/127

--- app/src/main/java/com/polzzak_android/presentation/feature/auth/signup/model/SignUpPage.kt
+++ app/src/main/java/com/polzzak_android/presentation/feature/auth/signup/model/SignUpPage.kt
@@ -8,3 +8,29 @@ enum class SignUpPage(val progressCount: Int?) {
     SET_NICKNAME(2),
     SET_PROFILE_IMAGE(3)
 }
+//
+//sealed interface SignUpPage {
+//    val progressCount: Int?
+//    val maxCount: Int
+//
+//    class Error : SignUpPage {
+//        override val progressCount = null
+//        override val maxCount = 0
+//    }
+//
+//    class TermsOfService : SignUpPage {
+//        override val progressCount = null
+//        override val maxCount = 0
+//    }
+//
+//    class SelectType : SignUpPage {
+//        override val progressCount = null
+//        override val maxCount: Int = 0
+//    }
+//
+//    class SelectParentType(override val progressCount: Int?, override val maxCount: Int) :
+//        SignUpPage
+//
+//    class SetNickName(override val progressCount: Int?, override val maxCount: Int) : SignUpPage
+//    class SetProfileImage(override val progressCount: Int?, override val maxCount: Int) : SignUpPage
+//}
